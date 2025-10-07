7a9dca0681762bd4890f0709cb3588f878dc58f7
Loan Nguyen <loannguyen@Loans-MacBook-Pro.local>
2025-10-07 23:26:06 +0700

    Upload

---
+++ code/Subject.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subject
+{
+    private String subject_id;
+    private String name;
+    private String instructor;
+    private List<Note> notes = new ArrayList<>();
+    private List<Assignment> assignments = new ArrayList<>();
+    public String get_subject_id()
+        return subject_id;
+    public void set_subject_id(String subject_id)
+        this.subject_id = subject_id;
+    public String get_name_subject()
+        return name;
+    public void set_name_subject(String name)
+        this.name = name;
+    public String get_instructor()
+        return instructor;
+    public void set_instructor(String instructor)
+        this.instructor = instructor;
+    public List<Note> get_notes()
+        return notes;
+    public void set_notes(List<Note> notes)
+        this.notes = notes;
+    public List<Assignment> get_assignments()
+        return assignments;
+    public void set_assignments(List<Assignment> assignments)
+        this.assignments = assignments;
+    public void add_note(Note note)
+        notes.add(note);
+    public void remove_note(Note note)
+        notes.remove(note);
+    public void add_assignment(Assignment assignment)
+        assignments.add(assignment);
+    public void remove_assignment(Assignment assignment)
+        assignments.remove(assignment);
+}
