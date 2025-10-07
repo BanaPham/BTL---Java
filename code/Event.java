bf131b88a362167c8521e6b4ceba87e07f5e5e9f
NhungNT27112005 <nhung@example.com>
2025-10-08 01:13:46 +0700

    mycode

--- code/Event.java
+++ code/Event.java
@@ -1,22 +1,15 @@
-
 package code;
-
 import java.util.Date;
-
-
 public class Event {
     private String title;
     private Date time;
-
     public Event(String title, Date time) {
         this.title = title;
         this.time = time;
     }
-
     public String getTitle() {
         return title;
     }
-
     public Date getTime() {
         return time;
     }
--- code/Note.java
+++ code/Note.java
@@ -1,8 +1,6 @@
-
 package code;
 import java.util.Date;
 import javax.security.auth.Subject;
-
 public class Note {
     private Subject related_subject;
     private String title;
@@ -15,48 +13,35 @@ public class Note {
         this.content = content;
         this.creationDate = creationDate;
     }
-
-    // Getter và Setter
     public Subject getRelated_subject() {
         return related_subject;
     }
-
     public void setRelated_subject(Subject related_subject) {
         this.related_subject = related_subject;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getContent() {
         return content;
     }
-
     public void setContent(String content) {
         this.content = content;
     }
-    
      public void addContent(String newContent) {
         this.content = newContent;
-    }
-     
+    } 
      public void removeContent() {
     this.content = null;
     }
-
     public Date getCreationDate() {
         return creationDate;
     }
-
     public void setCreationDate(Date creationDate) {
         this.creationDate = creationDate;
     }
-
-    
 }
 
