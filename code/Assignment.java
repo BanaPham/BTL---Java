b9c22f1f5b07627f3e9c7a0e6377105f2d3517cb
Loan Nguyen <loannguyen@Loans-MacBook-Pro.local>
2025-10-08 15:31:08 +0700

    Add new

--- code/Assignment.java
+++ code/Assignment.java
@@ -1,5 +1,67 @@
 package code;
-
-public class Assignment {
-    
+import java.util.Date;
+public class Assignment
+{
+    private Subject related_subject;
+    private String name;
+    private Date deadline;
+    private Status status;
+    public enum Status
+    {
+        Complete,
+        Pending
+    }
+    public Assignment(Subject related_subject, String name, Date deadline, Status status)
+    {
+        this.related_subject = related_subject;
+        this.name = name;
+        this.deadline = deadline;
+        this.status = status;
+    }
+    public Assignment()
+    {
+        this.related_subject = null;
+        this.name = "";
+        this.deadline = null;
+        this.status = Status.Pending;
+    }
+    public Subject getRelatedSubject()
+    {
+        return related_subject;
+    }
+    public void setRelatedSubject(Subject related_subject)
+    {
+        this.related_subject = related_subject;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public Date getDeadline()
+    {
+        return deadline;
+    }
+    public void setDeadline(Date deadline)
+    {
+        this.deadline = deadline;
+    }
+    public Status getStatus()
+    {
+        return status;
+    }
+    public void setStatus(Status status)
+    {
+        this.status = status;
+    }
+    public String getSubjectName()
+    {
+        if (related_subject != null)
+            return related_subject.getName();
+        else
+            return "(No Subject)";
+    }
 }
--- code/Subject.java
+++ code/Subject.java
@@ -1,5 +1,87 @@
 package code;
-
-public class Subject {
-    
+import java.util.ArrayList;
+import java.util.List;
+public class Subject
+{
+    private String subject_id;
+    private String name;
+    private String instructor;
+    private List<Note> notes;
+    private List<Assignment> assignments;
+    public Subject()
+    {
+        this.subject_id = "";
+        this.name = "";
+        this.instructor = "";
+        this.notes = new ArrayList<>();
+        this.assignments = new ArrayList<>();
+    }
+    public Subject(String subject_id, String name, String instructor)
+    {
+        this.subject_id = subject_id;
+        this.name = name;
+        this.instructor = instructor;
+        this.notes = new ArrayList<>();
+        this.assignments = new ArrayList<>();
+    }
+    public String getSubjectId()
+    {
+        return subject_id;
+    }
+    public void setSubjectId(String subject_id)
+    {
+        this.subject_id = subject_id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getInstructor()
+    {
+        return instructor;
+    }
+    public void setInstructor(String instructor)
+    {
+        this.instructor = instructor;
+    }
+    public List<Note> getNotes()
+    {
+        return notes;
+    }
+    public void setNotes(List<Note> notes)
+    {
+        this.notes = notes;
+    }
+    public List<Assignment> getAssignments()
+    {
+        return assignments;
+    }
+    public void setAssignments(List<Assignment> assignments)
+    {
+        this.assignments = assignments;
+    }
+    public void addNote(Note note)
+    {
+        if(!this.notes.contains(note))
+            this.notes.add(note);
+    }
+    public void removeNote(Note note)
+    {
+        if(this.notes.contains(note))
+            this.notes.remove(note);
+    }
+    public void addAssignment(Assignment assignment)
+    {
+        if(!this.assignments.contains(assignment))
+            this.assignments.add(assignment);
+    }
+    public void removeAssignment(Assignment assignment)
+    {
+        if(this.assignments.contains(assignment))
+            this.assignments.remove(assignment);
+    }
 }
