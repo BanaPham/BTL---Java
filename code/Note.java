6b2d2c82f861f8145894750b22bc8261deab2b90
nov_roses.27 <ntn27112005@gmail.com>
2025-10-28 10:27:26 +0700

    Code

--- code/Note.java
+++ code/Note.java
@@ -1,58 +1,46 @@
 package code;
 import java.util.Date;
-public class Note {
-    private String note_id;
+public class Note extends Object {
     private Subject related_subject;
-    private String title;
     private String content;
     private Date creationDate;
     // Note 
-    public Note(String note_id, Subject related_subject, String title, String content, Date creationDate) {
-        this.note_id = note_id;
-        this.related_subject = related_subject;
-        this.title = title;
-        this.content = content;
-        this.creationDate = creationDate;
-    }
-    // note_id
-    public String getNoteId(){
-        return note_id;
-    }
-    public void setNoteId (String node_id) {
-        this.note_id = node_id;
+    public Note(String id, Subject related_subject, String name, String content, Date creationDate) {
+        super(id, name);
+        setRelated_subject(related_subject);
+        setContent(content);
+        setCreationDate(creationDate);
     }
     // related_subject
     public Subject getRelated_subject() {
         return related_subject;
     }
     public void setRelated_subject(Subject related_subject) {
-        this.related_subject = related_subject;
-    }
-     // title
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
+        if (related_subject != null) {
+            this.related_subject = related_subject;
+        }
     }
     // content
     public String getContent() {
         return content;
     }
     public void setContent(String content) {
+        if(!content.equals(""))
         this.content = content;
     }
-     public void addContent(String newContent) {
+    public void addContent(String newContent) {
         this.content = newContent;
     } 
-     public void removeContent() {
-    this.content = null;
+    public void removeContent() {
+        this.content = null;
     }
     // creationDate
     public Date getCreationDate() {
         return creationDate;
     }
     public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
+        if (creationDate != null) {
+            this.creationDate = creationDate;
+        }
     }
 }
