3ff9bb087438464a041efbe1ccee057a80123b5d
nov_roses.27 <ntn27112005@gmail.com>
2025-10-28 10:30:30 +0700

    code

--- code/Schedule.java
+++ code/Schedule.java
@@ -1,42 +1,23 @@
 package code;
 import java.util.List;
 import java.util.ArrayList;
-public class Schedule {
-    private String schedule_id;
+public class Schedule extends Object{
     private Subject related_subject;
-    private String name;
     private List<Event> events;
     // Schedule
-    public Schedule(String schedule_id, Subject related_subject, String name) {
-        this.schedule_id = schedule_id;
-        this.related_subject = related_subject;
-        this.name = name;
-        this.events = new ArrayList<>();
+    public Schedule(String id, Subject related_subject, String name) {
+        super(id, name);
+        setRelated_schedule(related_schedule);
     }  
-    // schedule_id
-    public String getScheduleId(){
-        return schedule_id;
-    }
-    public void setScheduleId (String schedule_id) {
-        this.schedule_id = schedule_id;
-    }
     // related_subject
-     public Subject getRelated_subject() {
+    public Subject getRelated_subject() {
         return related_subject;
     } 
-     public void setRelated_subject(Subject related_subject) {
-        this.related_subject = related_subject;
+    public void setRelated_subject(Subject related_subject) {
+        if (related_subject != null) {
+            this.related_subject = related_subject;
+        }
     } 
-    // name
-    public String getName() {
-        return name;
-    }
-     public void setName(String name) {
-        this.name = name;
-    }
-     public void removeName() {
-        this.name = null;
-    }
     // Event 
     public List<Event> getEvents(){
         return events;
@@ -46,7 +27,7 @@ public class Schedule {
             events.add(event);
         }
     }
-     public void removeEvent(Event event) {
+    public void removeEvent(Event event) {
         if (this.events.contains(event))
             this.events.remove(event);
     }
